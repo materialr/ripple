d505538cf2e35a6a515754456201632f5bb38b10
Hendrik Volschenk <hendrikv@globalkinetic.com>
2017-11-02 10:53:16 +0200

    fix: move more code to utils. have a look at https://github.com/material-components/material-components-web/blob/master/packages/mdc-ripple/index.js to see what is implemented by default

--- src/adapter-utilities.js
+++ src/adapter-utilities.js
@@ -1,16 +1,40 @@
+const PASSIVE_EVENT_LISTENERS = ['touchstart'];
+const matches = (prototype => ['webkitMatchesSelector', 'msMatchesSelector', 'matches']
+  .filter(property => property in prototype).slice(-1)[0])(HTMLElement.prototype);
+
 let classNames = [];
+let cssVariables = {};
 
 export const addClass = updateClassNames => (className) => {
   classNames = [...classNames, className];
   updateClassNames(classNames);
 };
 
+export const computeBoundingRect = element => () => {
+  const { bottom, left, right, top } = element.getBoundingClientRect();
+  return { top, left, right, bottom, width: right - left, height: bottom - top };
+};
+
 export const deregisterInteractionHandler = element =>
   (type, handler) => element.removeEventListener(type, handler);
 
+export const isSurfaceActive = element => () => element[matches](':active');
+
 export const isUnbounded = centered => () => centered;
+
+export const registerInteractionHandler = element => (type, handler) =>
+  element.addEventListener(
+    type,
+    handler,
+    PASSIVE_EVENT_LISTENERS.includes(type) ? { passive: true } : null,
+  );
 
 export const removeClass = updateClassNames => (className) => {
   classNames = classNames.filter(currentClassName => currentClassName !== className);
   updateClassNames(classNames);
+};
+
+export const updateCssVariable = updateCssVariables => (variable, value) => {
+  cssVariables = { ...cssVariables, [variable]: value };
+  updateCssVariables(cssVariables);
 };
--- src/adapter-utilities.test.js
+++ src/adapter-utilities.test.js
@@ -1,9 +1,20 @@
-import { addClass, deregisterInteractionHandler, isUnbounded, removeClass } from './adapter-utilities';
+import {
+  addClass,
+  computeBoundingRect,
+  deregisterInteractionHandler,
+  isSurfaceActive,
+  isUnbounded,
+  registerInteractionHandler,
+  removeClass,
+  updateCssVariable,
+} from './adapter-utilities';
 
 const CLASS_NAME_1 = 'CLASS_NAME_1';
 const CLASS_NAME_2 = 'CLASS_NAME_2';
+const CSS_VARIABLE_2 = 'CSS_VARIABLE_2';
+const CSS_VARIABLE_VALUE_2 = 'CSS_VARIABLE_VALUE_2';
 
-test('\'addClass()\' adds a className and sends the list of classNames to \'updateClassNames()\'', () => {
+test('\'addClass()\' adds a className and sends the list to \'updateClassNames()\'', () => {
   const expectedFirst = [CLASS_NAME_1];
   const expectedSecond = [CLASS_NAME_1, CLASS_NAME_2];
   const updateClassNames = jest.fn();
@@ -13,6 +24,17 @@ test('\'addClass()\' adds a className and sends the list of classNames to \'upda
 
   expect(updateClassNames.mock.calls[0][0]).toEqual(expectedFirst);
   expect(updateClassNames.mock.calls[1][0]).toEqual(expectedSecond);
+});
+
+test('\'computeBoundingRect()\' returns the bounding box of the element and it\'s size', () => {
+  const GET_BOUNDING_CLIENT_RECT = jest.fn()
+    .mockReturnValue({ bottom: 10, left: 0, right: 10, top: 0 });
+  const element = { getBoundingClientRect: GET_BOUNDING_CLIENT_RECT };
+  const expected = { bottom: 10, height: 10, left: 0, right: 10, top: 0, width: 10 };
+
+  const actual = computeBoundingRect(element)();
+
+  expect(actual).toEqual(expected);
 });
 
 test('\'deregisterInteractionHandler()\' removes an event listener from the element', () => {
@@ -26,6 +48,24 @@ test('\'deregisterInteractionHandler()\' removes an event listener from the elem
   expect(REMOVE_EVENT_LISTENER).toBeCalledWith(TYPE, HANDLER);
 });
 
+test('\'isSurfaceActive()\' returns true when the surface is active', () => {
+  const matches = jest.fn().mockReturnValue(true);
+  const element = { matches };
+
+  const actual = isSurfaceActive(element)();
+
+  expect(actual).toBe(true);
+});
+
+test('\'isSurfaceActive()\' returns false when the surface is inactive', () => {
+  const matches = jest.fn().mockReturnValue(false);
+  const element = { matches };
+
+  const actual = isSurfaceActive(element)();
+
+  expect(actual).toBe(false);
+});
+
 test('\'isUnbounded()\' returns the \'centered\' property directly', () => {
   const CENTERED = 'CENTERED';
   const expected = CENTERED;
@@ -33,6 +73,28 @@ test('\'isUnbounded()\' returns the \'centered\' property directly', () => {
   const actual = isUnbounded(CENTERED)();
 
   expect(actual).toBe(expected);
+});
+
+test('\'registerInteractionHandler()\' adds a non-passive interaction handler', () => {
+  const ADD_EVENT_LISTENER = jest.fn();
+  const HANDLER = 'HANDLER';
+  const TYPE = 'TYPE';
+  const element = { addEventListener: ADD_EVENT_LISTENER };
+
+  registerInteractionHandler(element)(TYPE, HANDLER);
+
+  expect(ADD_EVENT_LISTENER).toBeCalledWith(TYPE, HANDLER, null);
+});
+
+test('\'registerInteractionHandler()\' adds a passive interaction handler', () => {
+  const ADD_EVENT_LISTENER = jest.fn();
+  const HANDLER = 'HANDLER';
+  const TYPE = 'touchstart';
+  const element = { addEventListener: ADD_EVENT_LISTENER };
+
+  registerInteractionHandler(element)(TYPE, HANDLER);
+
+  expect(ADD_EVENT_LISTENER).toBeCalledWith(TYPE, HANDLER, { passive: true });
 });
 
 test('\'removeClass()\' removes a classNames ands sends the list of classNames to \'updateClassNames()\'', () => {
@@ -45,4 +107,35 @@ test('\'removeClass()\' removes a classNames ands sends the list of classNames t
 
   expect(updateClassNames.mock.calls[0][0]).toEqual(expectedFirst);
   expect(updateClassNames.mock.calls[1][0]).toEqual(expectedSecond);
+});
+
+test('\'updateCssVariable()\' adds a css variable', () => {
+  const CSS_VARIABLE_1 = 'CSS_VARIABLE_1';
+  const CSS_VARIABLE_VALUE_1 = 'CSS_VARIABLE_VALUE_1';
+  const updateCssVariables = jest.fn();
+  const expectedFirst = { [CSS_VARIABLE_1]: CSS_VARIABLE_VALUE_1 };
+  const expectedSecond = {
+    [CSS_VARIABLE_1]: CSS_VARIABLE_VALUE_1,
+    [CSS_VARIABLE_2]: CSS_VARIABLE_VALUE_2,
+  };
+
+  updateCssVariable(updateCssVariables)(CSS_VARIABLE_1, CSS_VARIABLE_VALUE_1);
+  updateCssVariable(updateCssVariables)(CSS_VARIABLE_2, CSS_VARIABLE_VALUE_2);
+
+  expect(updateCssVariables.mock.calls[0][0]).toEqual(expectedFirst);
+  expect(updateCssVariables.mock.calls[1][0]).toEqual(expectedSecond);
+});
+
+test('\'updateCssVariable()\' changes a css variable', () => {
+  const CSS_VARIABLE_1 = 'CSS_VARIABLE_1';
+  const CSS_VARIABLE_VALUE_1_NEW = 'CSS_VARIABLE_VALUE_1_NEW';
+  const updateCssVariables = jest.fn();
+  const expected = {
+    [CSS_VARIABLE_1]: CSS_VARIABLE_VALUE_1_NEW,
+    [CSS_VARIABLE_2]: CSS_VARIABLE_VALUE_2,
+  };
+
+  updateCssVariable(updateCssVariables)(CSS_VARIABLE_1, CSS_VARIABLE_VALUE_1_NEW);
+
+  expect(updateCssVariables).toBeCalledWith(expected);
 });
